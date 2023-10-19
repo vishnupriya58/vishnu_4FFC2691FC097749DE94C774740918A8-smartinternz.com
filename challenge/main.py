59a970e6a186fdd42ede9aed82214a9e53444ec5
vishnupriya58 <148428735+vishnupriya58@users.noreply.github.com>
2023-10-19 12:24:32 +0530

    Add files via upload

---
+++ challenge/main.py
@@ -0,0 +1,10 @@
+def fact_rect(n):
+  if n==0 or n==1:
+    return 1
+  else:
+    return n*fact_rect(n-1)
+
+number=int(input("Enter a Value : "))
+res=fact_rect(number)
+
+print("The Factorial of {} is {}.".format(number,res))
